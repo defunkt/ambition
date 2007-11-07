15053bdbaf95a3d2a602212f3badd918de3b89fd
Chris Wanstrath <chris@ozmm.org>
2007-11-06 18:12:10 -0800

    remove, clean

--- test/databases/boot.rb
+++
@@ -1,3 +0,0 @@
-require File.dirname(__FILE__) + '/lib/load_fixtures'
-$:.unshift File.dirname(__FILE__) + '/../../lib'
-require 'ambition'
--- test/ruby_test.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + '/helper'
-
-context "Inline Ruby" do
-  xspecify "should know what to return" do
-    name = 'David'
-    sql = User.select { |u| name.nil? || u.name == name }.to_s
-    sql.should == "SELECT * FROM users WHERE (users.name = 'David')"
-  end
-end
