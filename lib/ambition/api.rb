5e4494fd1834bae4ef2a4c82950db3c2cede3b45
Matthew King <automatthew@gmail.com>
2007-11-17 19:42:14 -0600

    delete, don't just comment out, you silly boy.

--- lib/ambition/api.rb
+++ lib/ambition/api.rb
@@ -24,12 +24,6 @@ module Ambition
     end
 
     def slice(start, length = nil)
-      # if start.is_a? Range
-      #   length  = start.end
-      #   length -= 1 if start.exclude_end?
-      #   start = start.first
-      #   length -= start
-      # end
       context = ambition_context 
       context << Processors::Slice.new(context, start, length)
     end
