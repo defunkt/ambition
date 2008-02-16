4fa480c45ff43540fe411831b650fe35ba1ba7cf
Chris Wanstrath <chris@ozmm.org>
2008-02-16 02:46:33 -0800

    cache procs

--- lib/ambition/sexp_translator.rb
+++ lib/ambition/sexp_translator.rb
@@ -2,10 +2,14 @@ require 'parse_tree'
 
 module Ambition
   class SexpTranslator
+    @@block_cache = {}
+
     def self.translate(block)
-      klass = Class.new { define_method(:proc_to_method, block) }
-      body = ParseTree.translate(klass, :proc_to_method)[2][1..-1]
-      [:proc, *body]
+      @@block_cache[block.to_s] ||= begin
+        klass = Class.new { define_method(:proc_to_method, block) }
+        body = ParseTree.translate(klass, :proc_to_method)[2][1..-1]
+        [:proc, *body]
+      end
     end
   end
 end
