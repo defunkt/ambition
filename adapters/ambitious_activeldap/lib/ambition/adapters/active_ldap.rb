5e94044e4647d94f9846791290e88cc37b175125
Chris Wanstrath <chris@ozmm.org>
2008-04-26 05:11:48 -0400

    remove adapters from main repo

--- adapters/ambitious_activeldap/lib/ambition/adapters/active_ldap.rb
+++
@@ -1,8 +0,0 @@
-require 'ambition'
-require 'active_ldap'
-require 'ambition/adapters/active_ldap/query'
-require 'ambition/adapters/active_ldap/base'
-require 'ambition/adapters/active_ldap/select'
-
-ActiveLdap::Base.extend Ambition::API
-ActiveLdap::Base.ambition_adapter = Ambition::Adapters::ActiveLdap
